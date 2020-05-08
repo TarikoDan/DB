15b74fd1aa85d6b6daf8ae7cf894455fd2f30ef1
TarikoDan <62624974+TarikoDan@users.noreply.github.com>
2020-05-08 21:00:20 +0300

    2

--- MySQL/Second.sql
+++ MySQL/Second.sql
@@ -1,26 +1,54 @@
 # create database if not exists cars;
 use lessondb;
-create table cars(
-    id int not null primary key auto_increment,
-    model varchar(255),
-    power int,
-    year int(4),
+create table cars
+(
+    id           int not null primary key auto_increment,
+    model        varchar(255),
+    power        int,
+    year         int(4),
     manufacturer varchar(255),
-    type varchar(255),
-    volume int
+    type         varchar(255),
+    volume       int
 );
-insert into cars (manufacturer, model, power, year,  type, volume)
-    values
-('BMW','530',250,2015,'sedan',3000),
-('BMW','M320',250,2019,'hatchback',2000),
-('BMW','X5',350,2011,'SUV',3500),
-('Mercedes','S500',350,2009,'sedan',5000),
-('Mercedes','E200',150,2016,'sedan',1800),
-('Mercedes','GL',550,2015,'SUV',5500),
-('Audi','A5',250,2014,'universal',2300),
-('Audi','A1',80,2016,'hatchback',1100),
-('Audi','Q8',450,2019,'SUV',4000),
-('VW','B6',190,2015,'universal',1900),
-('VW','T5',180,2016,'bus',2500),
-('VW','Tuareg',280,2018,'SUV',3000);
+insert into cars (manufacturer, model, power, year, type, volume)
+values ('BMW', '530', 250, 2015, 'sedan', 3000),
+       ('BMW', 'M320', 250, 2019, 'hatchback', 2000),
+       ('BMW', 'X5', 350, 2011, 'SUV', 3500),
+       ('Mercedes', 'S500', 350, 2009, 'sedan', 5000),
+       ('Mercedes', 'E200', 150, 2016, 'sedan', 1800),
+       ('Mercedes', 'GL', 550, 2015, 'SUV', 5500),
+       ('Audi', 'A5', 250, 2014, 'universal', 2300),
+       ('Audi', 'A1', 80, 2016, 'hatchback', 1100),
+       ('Audi', 'Q8', 450, 2019, 'SUV', 4000),
+       ('VW', 'B6', 190, 2015, 'universal', 1900),
+       ('VW', 'T5', 180, 2016, 'bus', 2500),
+       ('VW', 'Tuareg', 280, 2018, 'SUV', 3000);
 
+select *
+from cars
+where year between 2010 and 2020;
+
+select * from cars
+where volume <2000 and power > 100;
+
+select * from cars
+where manufacturer = 'BMW' and power > 100 and  volume < 3000;
+
+select count(distinct manufacturer) from cars;
+
+select
+     manufacturer
+    ,count(manufacturer)
+                    from cars
+group by manufacturer;
+
+select
+    count(id) as count,
+    concat(manufacturer,' ', model,' ',volume)
+from cars
+group by volume
+order by volume;
+
+
+update cars
+set type = 'sportcar' where power > 300;
